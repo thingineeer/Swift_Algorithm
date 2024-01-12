4ebbaf6f4b631020d332407899a228999196a5a9
thingineeer <88179341+thingineeer@users.noreply.github.com>
2024-01-12 12:00:08 +0900

    스택
    스택의 기본 명령어들을 알아봤습니다.

---
+++ BOJ/10828.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+var stack: [String] = []
+
+enum Command: String {
+    case push
+    case pop
+    case size
+    case empty
+    case top
+}
+
+let x = Int(readLine()!)!
+
+for _ in 0..<x {
+    let input = readLine()!.split(separator: " ").map { String($0) }
+    
+    guard let command = Command(rawValue: input[0]) else {
+        fatalError("Error")
+    }
+    
+    switch command {
+    case .push:
+        stack.append(input[1])
+    case .pop:
+        print(stack.isEmpty ? "-1" : stack.removeLast())
+    case .size:
+        print(stack.count)
+    case .empty:
+        print(stack.isEmpty ? "1" : "0")
+    case .top:
+        let result = stack.isEmpty ? "-1" : stack.last!
+        print(result)
+    }
+}
