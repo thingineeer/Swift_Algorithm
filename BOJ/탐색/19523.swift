403b4542f8311210e9ba265a72394a0162ef8f6c
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-07-31 00:11:13 +0900

    [백준] #19532번 - 완전 탐색

---
+++ BOJ/탐색/19523.swift
@@ -0,0 +1,26 @@
+//
+//  19532.swift
+//  수학은 비대면강의입니다 B2
+//
+//  Created by 이명진 on 31/7/24.
+//
+
+// Ax + By = C
+// Dx + Ey = F
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+var result: (Int, Int) = (0, 0)
+
+for X in -999...999 {
+    for Y in -999...999 {
+        let A = input[0], B = input[1], C = input[2], D = input[3], E = input[4], F = input[5]
+        if A * X + B * Y == C && D * X + E * Y == F {
+            result = (X, Y)
+            break
+        }
+    }
+}
+
+print(result.0, result.1)
