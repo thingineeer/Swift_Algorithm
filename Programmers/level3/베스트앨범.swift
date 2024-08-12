03eef7c6b2ab98de54381fe6a3101b69c578cfdf
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-08-12 13:21:30 +0900

    refactor: 조건문 안쓰는 방법으로 코드 개선

--- Programmers/level3/베스트앨범.swift
+++ Programmers/level3/베스트앨범.swift
@@ -28,7 +28,7 @@ func getGenrePlayCount(_ genres: [String], _ record: [Record], _ genre: Set<Stri
     return specificGenre.sorted { $0.1 > $1.1 }
 }
 
-   
+
 func solution(_ genres: [String], _ plays: [Int]) -> [Int] {
     
     let genre = Set(genres)
@@ -38,17 +38,15 @@ func solution(_ genres: [String], _ plays: [Int]) -> [Int] {
     
     for (genreName, _) in specificGenre {
         let filterList = record.filter { $0.name == genreName }.sorted { $0.count > $1.count }
-
         
-        // prefix 보다 시간 복잡도가 더 낮았다 (프로그래머스 환경 에서는)
-        if filterList.count > 1 {
-            for i in 0...1 {
-                result.append(filterList[i].index)
-            }
-        } else {
-            result.append(filterList[0].index)
+        
+        // prefix(2) 보다 시간 복잡도가 더 낮았다 (프로그래머스 환경 에서는)
+        // 기존 조건문을 변경하고 싶어서 아래 처럼 변경
+        // 기존 방식과 거의 유사하지만, `prefix(2)`의 사용을 피하며 코드 간결화
+        for i in 0..<min(2, filterList.count) {
+            result.append(filterList[i].index)
         }
     }
     
-    return result
+    return result   
 }
