6a9260588efadf1ad6d4cb8255bcd9acd6d5e4ac
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-08-13 14:13:54 +0900

    feat: 세자리씩 붙혀서 수 정렬

---
+++ Programmers/level3/가장_큰_수.swift
@@ -0,0 +1,16 @@
+// 가장 큰 수.swift level2
+
+import Foundation
+
+func solution(_ numbers: [Int]) -> String {
+    
+    let num = numbers.map { (String($0) + String($0) + String($0), String($0)) }.sorted { $0.0 > $1.0 }.map { $1 }
+    
+    var result = ""
+    
+    for str in num {
+        result += str
+    }
+    
+    return result
+}
