09bd722958947a8ccc9663bdfd5e9aa36050c0fe
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-10-27 00:00:47 +0900

    [백준] #1290번 - 탐색 재풀이 S2

--- BOJ/탐색/1260.swift
+++ BOJ/탐색/1260.swift
@@ -1,18 +1,23 @@
 /// https://www.acmicpc.net/problem/1260
+//
+//  1260.swift
+//  DFS와 BFS - S2
+//
+//  Created by 이명진 on 10/27/24.
+//
+
 import Foundation
 
-let input = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
-
+let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N = input[0]
 let M = input[1]
 let V = input[2]
 
-var graph: [[Int]] = Array(repeating: [], count: N+1)
-var visited = [Bool](repeating: false, count: graph.count)
-var queue: [Int] = []
+var graph: [[Int]] = Array(repeating: [], count: N + 1)
+var visited = Array(repeating: false, count: N + 1)
 
 for _ in 0..<M {
-    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
     graph[input[0]].append(input[1])
     graph[input[1]].append(input[0])
 }
@@ -21,27 +26,29 @@ for i in 0..<N+1 {
     graph[i].sort()
 }
 
-func dfs(v: Int, graph: [[Int]], visited: inout [Bool]) {
+func dfs(v: Int, visited: inout [Bool], graph: [[Int]]) {
+    
     visited[v] = true
     print(v, terminator: " ")
     
     for i in graph[v] {
         if !visited[i] {
-            dfs(v: i, graph: graph, visited: &visited)
+            dfs(v: i, visited: &visited, graph: graph)
         }
     }
 }
 
-func bfs(v: Int, graph: [[Int]], visited: inout [Bool]) {
+func bfs(V: Int, visited: inout [Bool], graph: [[Int]]) {
+    visited[V] = true
     
-    visited[v] = true
-    queue.append(v)
+    var queue: [Int] = [V]
     
     while !queue.isEmpty {
-        let x = queue.removeFirst()
-        print(x, terminator: " ")
+        let v = queue.removeFirst()
         
-        for i in graph[x] {
+        print(v, terminator: " ")
+        
+        for i in graph[v] {
             if !visited[i] {
                 queue.append(i)
                 visited[i] = true
@@ -50,8 +57,9 @@ func bfs(v: Int, graph: [[Int]], visited: inout [Bool]) {
     }
 }
 
-dfs(v: V ,graph: graph, visited: &visited)
+dfs(v: V, visited: &visited, graph: graph)
+
+visited = Array(repeating: false, count: N + 1)
 print()
 
-visited = [Bool](repeating: false, count: graph.count)
-bfs(v: V ,graph: graph, visited: &visited)
+bfs(V: V, visited: &visited, graph: graph)
