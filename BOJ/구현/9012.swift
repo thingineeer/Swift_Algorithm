31951f0c51800fb30002b5a2ae847854f8f2e68c
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-10-28 02:56:58 +0900

    [Setting] - 폴더 이동

--- BOJ/구현/9012.swift
+++ BOJ/구현/9012.swift
@@ -1,6 +1,5 @@
 import Foundation
-
-// 강제 언래핑이 아닌 flatMap과 바인딩으로 언래핑 
+ 
 if let x = readLine().flatMap({ Int($0) }) {
     for _ in 0..<x {
         var count = 0
@@ -20,22 +19,3 @@ if let x = readLine().flatMap({ Int($0) }) {
         }
     }
 }
-
-//let x = Int(readLine()!)!
-//
-//for _ in 0..<x {
-//    var count: Int = 0
-//    let nums = Array(readLine()!).map {String($0)}
-//    for num in nums {
-//        if num == "(" {
-//            count += 1
-//        } else {
-//            count -= 1
-//            if count < 0 {
-//                break
-//            }
-//        }
-//    }
-//    
-//    count == 0 ? print("YES") : print("NO")
-//}
